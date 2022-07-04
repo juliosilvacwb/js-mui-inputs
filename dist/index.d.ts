d8e6cc94d280c284965dedd344fc44c3d4a5643d
Julio Silva <julio@hoxo.com.br>
2022-07-04 06:33:50 -0300

    Implementação inicial lib component react com storybook

---
+++ dist/index.d.ts
@@ -0,0 +1,22 @@
+import { MouseEventHandler, ChangeEventHandler } from 'react';
+
+interface ButtonProps {
+    text?: string;
+    primary?: boolean;
+    disabled?: boolean;
+    size?: "small" | "medium" | "large";
+    onClick?: MouseEventHandler<HTMLButtonElement>;
+}
+
+interface InputProps {
+    id?: string;
+    label?: string;
+    error?: boolean;
+    message?: string;
+    success?: boolean;
+    disabled?: boolean;
+    placeholder?: string;
+    onChange?: ChangeEventHandler<HTMLInputElement>;
+}
+
+export { ButtonProps, InputProps };
---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+export * from './Button/Button.types';
+export * from './Button/Button';
+export * from './Input/Input.types';
+export * from './Input/Input';
